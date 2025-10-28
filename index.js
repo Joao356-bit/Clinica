21b0d65c9e68f7ef378adf0496b2a11673044d19
Joao356-bit <jonasluis3401@gmail.com>
2025-10-28 18:59:25 -0300

    FIX FINAL: Remove Swagger para garantir deploy

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-
 require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
-const swaggerUi = require('swagger-ui-express');
-
 const app = express();
 app.use(express.json());
-
 const pacientesRoutes = require('./routes/pacientesRoutes.js');
 app.use('/pacientes', pacientesRoutes); 
 
 app.get('/', (req, res) => {
     res.send('API da Clínica de Fisioterapia no ar!');
 });
-
-const simpleSwaggerDoc = {
-  openapi: '3.0.0',
-  info: {
-    title: 'TESTE SIMPLES DE SWAGGER',
-    version: '1.0.0',
-    description: 'Se isto aparecer, o problema é o JSON antigo.'
-  },
-  paths: {} 
-};
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(simpleSwaggerDoc));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
